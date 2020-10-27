272868fad3b8f540125e11a2bd18b3b5939d3d73
yaoyaoziiyandex <yaoyaoziiyandex@gmail.com>
2020-10-27 11:30:52 +0000

    Log

--- src/main/java/yaoyaozii/App.java
+++ src/main/java/yaoyaozii/App.java
@@ -1,13 +1,35 @@
 package yaoyaozii;
 
-/**
- * Hello world!
- *
- */
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+  
+  public static void main( String[] args ) 
+  {
+
+    // java -Djava.util.logging.config.file=/home/user/config/logging.properties
+    
+    //Dynamic way, cannot call inside static method
+    //System.out.println(getClass().getClassLoader().getResource("logging.properties"));
+    
+    // Static way
+    System.out.println(App.class.getClassLoader().getResource("logging.properties"));
+
+    LOGGER.setLevel(Level.SEVERE);
+    LOGGER.severe("Info Log");
+    LOGGER.warning("Info Log");
+    LOGGER.info("Info Log");
+    LOGGER.finest("Really not important");
+    
+    LOGGER.setLevel(Level.FINEST);
+    LOGGER.severe("Info Log");
+    LOGGER.warning("Info Log");
+    LOGGER.info("Info Log");
+    LOGGER.finest("Really not important");
+    
+    System.out.println( "Done!" );
+  }
 }
