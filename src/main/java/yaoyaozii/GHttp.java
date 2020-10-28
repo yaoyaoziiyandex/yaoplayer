00b080f0de6b48e5ad9c612e841a3ef7dec2d54c
yaoyaoziiyandex <yaoyaoziiyandex@gmail.com>
2020-10-28 08:02:24 +0000

    update

--- src/main/java/yaoyaozii/GHttp.java
+++ src/main/java/yaoyaozii/GHttp.java
@@ -3,36 +3,22 @@ package yaoyaozii;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+
 public class GHttp 
 {
-  private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+  private final static Logger LOGGER = Logger.getLogger(GHttp.class.getName());
 
   //static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
   //static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
   public static void run() 
   {
-
-    // java -Djava.util.logging.config.file=/home/user/config/logging.properties
-    
-    //Dynamic way, cannot call inside static method
-    //System.out.println(getClass().getClassLoader().getResource("logging.properties"));
-    
-    // Static way
-    System.out.println(GHttp.class.getClassLoader().getResource("logging.properties"));
-
-    LOGGER.setLevel(Level.SEVERE);
-    LOGGER.severe("Info Log");
-    LOGGER.warning("Info Log");
-    LOGGER.info("Info Log");
-    LOGGER.finest("Really not important");
-    
     LOGGER.setLevel(Level.FINEST);
     LOGGER.severe("Info Log");
     LOGGER.warning("Info Log");
     LOGGER.info("Info Log");
     LOGGER.finest("Really not important");
-    
-    System.out.println( "Done!" );
   }
 }
--- src/main/java/yaoyaozii/YLog.java
+++ src/main/java/yaoyaozii/YLog.java
@@ -9,27 +9,10 @@ public class YLog
 
   void run() 
   {
-
-    // java -Djava.util.logging.config.file=/home/user/config/logging.properties
-    
-    //Dynamic way, cannot call inside static method
-    System.out.println("getClass().getClassLoader().getResource(\"logging.properties\")): " + getClass().getClassLoader().getResource("logging.properties"));
-    
-    // Static way
-    //System.out.println(App.class.getClassLoader().getResource("logging.properties"));
-
     //LOGGER.setLevel(Level.SEVERE);
     LOGGER.severe("Info Log");
     LOGGER.warning("Info Log");
     LOGGER.info("Info Log");
     LOGGER.finest("Really not important");
-    
-    /*
-    LOGGER.setLevel(Level.FINEST);
-    LOGGER.severe("Info Log");
-    LOGGER.warning("Info Log");
-    LOGGER.info("Info Log");
-    LOGGER.finest("Really not important");
-    */
   }
 }
